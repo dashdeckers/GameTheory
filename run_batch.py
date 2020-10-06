64a98902be4d10afd27699bc920ab37a27c0224f
Travis Hammond <dashdeckers@gmail.com>
2020-10-06 17:14:11 +0200

    Implemented batch run

--- run_batch.py
+++ run_batch.py
@@ -1,24 +1,22 @@
-from model import GTModel
-from parameters import params
+"""Handles batch runs.
 
-import matplotlib.pyplot as plt
+No need to ever change anything here, just use parameters.py to set up the
+experiment, run run_batch.py to execute it, and then run analysis.py to
+visualize it.
+"""
+
+from model import GTModel
+from parameters import params, var_params, iterations, max_steps, run_name
+
 from mesa.batchrunner import BatchRunner
 from mesa.datacollection import DataCollector
 import pandas as pd
 
-# Define parameters
-var_params = {
-    'i_n_agents': [60],
-    'd': [0.5],
-    # 'k': range(-2, 1),
-    # 'p': [i/10 for i in range(1, 11)],
-}
+
+# Don't change this
 for key, val in var_params.items():
     params.pop(key)
 
-run_name = 'tSNE'
-iterations = 1
-max_steps = 1000
 
 # Run experiments
 batch_runner = BatchRunner(
@@ -32,8 +30,8 @@ batch_runner = BatchRunner(
     # collect step data
     model_reporters={'DC': lambda m: m.datacollector},
 )
-
 batch_runner.run_all()
+
 
 # Collect data
 data = batch_runner.get_model_vars_dataframe()
@@ -46,20 +44,7 @@ for i in range(len(data['DC'])):
         run_data = data['DC'][i].get_model_vars_dataframe()
         step_data = step_data.append(run_data, ignore_index=True)
 
+
+# Save data
 data.to_csv(f'data_{run_name}.csv')
 step_data.to_csv(f'step_data_{run_name}.csv')
-
-
-# Plot data
-def n_plots(start, n_plots, iterations, max_steps, step_data):
-    for i in range(start, n_plots * iterations, iterations):
-        step_data[i * max_steps: (i+1) * max_steps].drop(
-            # Don't need these columns
-            columns=['perc_cooperative_actions', 'n_agents',
-                     'n_friendlier', 'n_aggressive']
-        ).plot()
-
-        plt.show()
-
-
-# n_plots(0, 3, iterations, max_steps, step_data)
