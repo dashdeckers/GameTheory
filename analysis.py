a7be00eec1a7591ddebd5a70646971ec6cd07096
Travis Hammond <dashdeckers@gmail.com>
2020-10-18 14:29:12 +0200

    Fixed analysis for huge datasets (it now reads in chunks)

--- analysis.py
+++ analysis.py
@@ -1,10 +1,7 @@
 """Visualize the data gathered from a batch run.
 
-Must use the same iterations, max_steps and run_name parameter value as the
-last batch run used. Could fix this for multiple different datasets by saving
-the experiment values as JSON along with the datasets themselves.
-
-Problem for later though.
+Uses the same run_name as defined in parameters.py but you can also just
+redefine the variable to plot a different dataset.
 """
 from parameters import strategy_colors, run_name
 import matplotlib.pyplot as plt
@@ -26,86 +23,83 @@ def get_params(run_name):
     return params
 
 
-def get_data(run_name):
+def get_data(params, column_filter=None):
+    # Read the data in chunks (one chunk is all iterations of a setting)
+    run_name = params['run_name']
+    chunksize = params['max_steps'] * params['iterations']
+
     data = pd.read_csv(f'{run_name}_data.csv')
-    step_data = pd.read_csv(f'{run_name}_step_data.csv')
+    for step_data_chunk in pd.read_csv(
+                f'{run_name}_step_data.csv',
+                usecols=column_filter,
+                chunksize=chunksize,
+            ):
 
-    # Fix for the FutureWarning about pairwise comparisons from numpy
-    data.set_index([data.columns.values[0]], inplace=True)
-    data.index.names = [None]
+        # Fix first column bug in case it is a problem
+        first_column = step_data_chunk.columns.values[0]
+        if first_column == 'Unnamed: 0':
+            step_data_chunk.set_index([first_column], inplace=True)
+            step_data_chunk.index.names = [None]
 
-    step_data.set_index([step_data.columns.values[0]], inplace=True)
-    step_data.index.names = [None]
-    return data, step_data
+        yield (data, step_data_chunk)
 
 
-def plot(params, data, step_data, plot='strategies', average=True):
+def plot(params, plot_list='strategies', average=True):
     """Plots the data.
 
     Just pass the relevant data and step_data and specify what to plot.
-    The 'plot' parameter can be either 'strategies', 'cooperation' or the name
-    of a specific column to plot. Toggle average to plot either the averages
-    across iterations/runs or each single iteration/run.
+    The 'plot' parameter can be either 'strategies', or a list of column names
+    to plot. Toggle average to plot either the averages across iterations/runs
+    or each single iteration/run.
     """
-    if plot == 'cooperation':
-        columns = ['perc_cooperative_actions']
-    elif plot == 'strategies':
-        columns = [
-            c for c in step_data.columns.to_list() if c not in [
+    def column_filter(column):
+        if plot_list == 'strategies':
+            return column not in [
                 'perc_cooperative_actions', 'n_agents',
                 'n_friendlier', 'n_aggressive', 'avg_agent_age',
                 'n_neighbors', 'avg_delta_energy',
             ]
-        ]
-    else:
-        columns = plot
+        else:
+            return column in plot_list
 
-    if average:
-        # split into one dataframes per setting (parameter configuration)
-        split_size = params['max_steps'] * params['iterations']
-        setting_splits = [
-            step_data.loc[i:i+split_size-1, :]
-            for i in range(0, len(step_data), split_size)
-        ]
-        # assert all(len(s) == split_size for s in setting_splits)
-        # assert len(setting_splits) == params['n_settings']
+    data_iter = get_data(params, column_filter=column_filter)
+    for idx, (data, step_data) in enumerate(data_iter):
 
-        for idx, setting in enumerate(setting_splits):
+        if average:
             # get the parameter values we are plotting for
             data_idx = idx * params['iterations']
             setting_values = list(
                 data.loc[data_idx][params['var_params'].keys()].items()
             )
             # plot the mean across iterations
-            setting[columns].groupby(
-                setting.index % params['max_steps']
+            step_data.groupby(
+                step_data.index % params['max_steps']
             ).mean().plot(color=strategy_colors)
-            plt.title(f'Showing average {plot} for:\n{setting_values}')
+            plt.title(f'Showing average {plot_list} for:\n{setting_values}')
             plt.legend(ncol=2)
             plt.show()
 
-    else:
-        for i in range(0, params['iterations'] * params['n_settings']):
-            # get the parameter values we are plotting for
-            setting_values = list(
-                data.loc[i][params['var_params'].keys()].items()
-            )
-            # plot the values
-            rows = slice(i * params['max_steps'], (i+1) * params['max_steps'])
-            step_data[rows][columns].plot(color=strategy_colors)
-            plt.title(f'Showing {plot} for:\n{setting_values}')
-            plt.legend(ncol=2)
-            plt.show()
+        else:
+            for i in range(0, params['iterations']):
+                # get the parameter values we are plotting for
+                setting_values = list(
+                    data.loc[i][params['var_params'].keys()].items()
+                )
+                # plot the values
+                rows = slice(
+                    i * params['max_steps'],
+                    (i+1) * params['max_steps']
+                )
+                step_data[rows].plot(color=strategy_colors)
+                plt.title(f'Showing {plot_list} for:\n{setting_values}')
+                plt.legend(ncol=2)
+                plt.show()
 
 
 if __name__ == '__main__':
-    # Read data
     # run_name = 'density'  # noqa
-    data, step_data = get_data(run_name)
+    columns = ['perc_cooperative_actions', 'avg_delta_energy']
     params = get_params(run_name)
 
-    # Plot data
-    print(data.head())
-    plot(params, data, step_data, 'strategies')
-    # plot(params, data, step_data, 'strategies', average=False)
-    plot(params, data, step_data, 'perc_cooperative_actions')
+    plot(params, plot_list=columns)
+    plot(params, plot_list='strategies')
